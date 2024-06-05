55df5c070bc01247b722a69751df6e8f0cdd026b
leroyjdahl <leroy@appryse.io>
2024-06-05 10:22:07 -0500

    Initial project

---
+++ assignment/scripts/1-function-practice.js
@@ -0,0 +1,103 @@
+console.log('***** Function Practice *****')
+
+// Add the required code to complete the functions below
+// After every function, use a console log to call the function
+// to test it and display the result
+
+// 1 (example). Function to return 'Hello World!'
+function hello() {
+  return 'Hello World!';
+}
+// Call the function, and console log the output to test
+console.log('Test - should say "Hello World!"', hello());
+
+
+// 2. Complete the function, so that it returns a personalized hello.  For example: 'Hello, Jo!'
+function helloName(name) {
+  return;
+}
+// Remember to call the function, and console log the output to test.
+
+
+// 3. Complete the function, so that it: takes in two numbers, adds them together, and returns the result.
+function addNumbers(firstNumber) {
+  return firstNumber + secondNumber;
+}
+// Call the function, and console log the output to test.
+
+
+// 4. Complete the function, so that it: takes in three numbers, multiplies them together, and return the result.
+function multiplyThree() {
+
+}
+// Call the function, and console log the output.
+
+
+// 5. Complete the function, so that it will return true if the number passed in is positive,
+// (ie: if it's greater than zero).
+// Otherwise, it should return false.
+function isPositive(number) {
+  if (number > 0) {
+    return;
+  }
+  return;
+}
+// Call the function, and console log the output to test.
+// In this case, you'll want to test it more than once.
+// There should be a separate console.log statement for each possible outcome,
+// So, you'll need at least one for true and one for false.
+// We've provided one of them for you, as an example.
+console.log('Testing isPositive, with input 7.  Output should be: true', isPositive(7));
+
+
+// 6. Complete the function, so that it returns the _last_ item in an array. If the 
+//    array is empty, it should return `undefined`.
+function getLast(array) {
+
+}
+// Call the function, and console log the output to test.
+// Make sure you test all possible outcomes.
+// In this case, you'll need to test it more than once to do that.
+
+// 7. Complete the function, to find a value in an array.
+// Return true if the value is found and false otherwise.
+// Use a loop.
+// DO NOT use: Array.includes, Array.indexOf, or Array.find 
+function find(value, array) {
+
+}
+// Call the function, and console log the output to test.
+// Make sure you test all of the different scenarios.
+// In this case, you'll need to test it more than once to do that.
+
+// ----------------------
+// Stretch Goals
+// ----------------------
+// 8. Complete the function, so that it will check if a letter is the first letter in a string.
+// Return true if it is.  Otherwise, return false.
+function isFirstLetter(letter, string) {
+
+}
+// Call the function, and console log the output to test.
+// Make sure you test the different possibilities.
+
+// 9. Complete the function, so that it takes in array and returns the sum of all numbers in the array.
+function sumAll() {
+  let sum = 0
+  // TODO: loop to add items from the array to the sum.
+  return sum;
+}
+// Call the function, and console log the output to test.
+// Make sure you test different scenarios.
+
+// 10. Write a function that returns a new array of all positive (greater than zero)
+//     numbers contained in an input array. If there are no positive numbers
+//     return an empty array. Note: The input array should not change.
+
+// Call the function, and console log the output to test.
+// Make sure you fully test the different scenarios.
+
+
+// 11. Pick a problem from Edabit(https://edabit.com/) or 
+//     CodeWars(https://www.codewars.com/). Then describe it 
+//     here in a comment, write the function, and test it!
